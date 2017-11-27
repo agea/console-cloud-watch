d8969389f797c0f438a08ab17ca281771fc3042a
Andrea Agili <agea@drwolf.it>
2017-11-27 09:47:22 +0100

    :tada::rocket: initial commit, with everything working, version 0.0.1

---
+++ README.md
@@ -0,0 +1,64 @@
+# Console Watch
+
+Send your browser errors and console message to AWS CLoudWatch
+
+## Setup
+
+ - Login to AWS console
+ - Open CloudWatch
+ - Create a Log Group (Set up a retention period to save space)
+ - Open IAM
+ - Create a new user with secret and access key
+ - Assign the user a new policy wich will be able to write logs and create log streams (set your region and log group name):
+ ```json
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "VisualEditor0",
+            "Effect": "Allow",
+            "Action": [
+                "logs:CreateLogStream",
+                "logs:PutLogEvents"
+            ],
+            "Resource": [
+                "arn:aws:logs:AWS_REGION:*:log-group:LOG_GROUP:*:*",
+                "arn:aws:logs:AWS_REGION:*:log-group:LOG_GROUP"
+            ]
+        }
+    ]
+}
+ ```
+ - beware that the credentials will be exposed in the browser so don't assign other permissions to this user.
+
+## Get Console Watch
+
+```
+npm install console-watch
+bower install console-watch
+
+https://cdn.rawgit.com/agea/console-watch/0.0.1/consolewatch-all.min.js
+
+```
+`dist/consolewatch-all.min.js` contains all the dependencies (and a custom build of AWS SDK with only the CloudWatch service), 
+if you already include AWS SDK you may use `dist/dist/consolewatch.min.js` directly, note that you have alsto to include `fingerprintjs2` and `stacktrace-js`
+
+
+
+## Usage
+Include `consolewatch.js` in your page, configure your parameters and you are done:
+
+```javascript
+    ConsoleWatch(AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_REGION, LOG_GROUP);
+```
+
+You may also pass 3 optional configuration parameters:
+
+- `levels` (default: `['error']`): an array of strings with the console levels to intercept
+- `timeout` (default: `10000`): milliseconds between calls to CloudWatch
+- `mute` (default: `false`): if set to `true`, intercepted message will not be shown in console
+
+Example: 
+```javascript
+    ConsoleWatch(AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_REGION, LOG_GROUP,['warn','error'],30000,true);
+```
---
+++ consolewatch.js
@@ -0,0 +1,99 @@
+(function (name, context, definition) {
+    'use strict'
+    if (typeof window.define === 'function' && window.define.amd) { window.define(definition) } else if (typeof module !== 'undefined' && module.exports) { module.exports = definition() } else if (context.exports) { context.exports = definition() } else { context[name] = definition() }
+})('ConsoleWatch', this, function () {
+
+    return function (aws_access_key_id, aws_secret_access_key, region, group, levels, timeout, mute) {
+        var logEvents = [];
+        var cloudwatchlogs = new AWS.CloudWatchLogs({
+            accessKeyId: aws_access_key_id, secretAccessKey: aws_secret_access_key, region: region
+        });;
+
+        var stream = localStorage.getItem('ConsoleWatch:stream');
+
+        if (stream) {
+            init();
+        } else {
+            if (typeof (Fingerprint2) == 'undefined') {
+                function guid() {
+                    function s4() {
+                        return Math.floor((1 + Math.random()) * 0x10000)
+                            .toString(16)
+                            .substring(1);
+                    }
+                    return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+                        s4() + '-' + s4() + s4() + s4();
+                }
+                createStream(guid());
+            } else {
+                new Fingerprint2().get(function (result, components) {
+                    createStream(stream);
+                });
+            }
+        }
+        window.onerror = function (msg, file, line, col, error) {
+            StackTrace.fromError(error).then(function (stackFrames) {
+                var stringifiedStack = msg + '\n' + file + '\n' + stackFrames.map(function (sf) {
+                    return sf.toString();
+                }).join('\n');
+                logEvents.push({
+                    message: stringifiedStack,
+                    timestamp: new Date().getTime()
+                });
+            });
+        };
+
+        if (typeof (console) !== 'undefined') {
+            (levels || ['error']).forEach(function (level) {
+                var originalFn = console[level];
+                console[level] = function (msg) {
+                    logEvents.push({
+                        message: msg,
+                        timestamp: new Date().getTime()
+                    });
+                    if (!mute) {
+                        originalFn(msg);
+                    }
+                }
+            });
+        }
+
+        function init() {
+            setInterval(function () {
+                var pendingEvents = logEvents.splice(0);
+                if (pendingEvents.length) {
+                    cloudwatchlogs.putLogEvents({
+                        logEvents: pendingEvents,
+                        sequenceToken: localStorage.getItem('ConsoleWatch:sequenceToken'),
+                        logGroupName: group,
+                        logStreamName: stream
+                    }, function (err, data) {
+                        if (err) {
+                            logEvents = pendingEvents.concat(logEvents);
+                            if (err.code == "InvalidSequenceTokenException") {
+                                localStorage.setItem('ConsoleWatch:sequenceToken', err.message.split('The next expected sequenceToken is: ')[1])
+                            }
+                        }
+                        if (data) {
+                            localStorage.setItem('ConsoleWatch:sequenceToken', data.nextSequenceToken)
+                        }
+                    });
+                }
+            }, timeout || 10000);
+        }
+
+        function createStream(stream) {
+            cloudwatchlogs.createLogStream({
+                logGroupName: group,
+                logStreamName: stream
+            }, function (err, data) {
+                if (err && err.code !== 'ResourceAlreadyExistsException') {
+                    console.log(err, err.stack);
+                } else {
+                    localStorage.setItem('ConsoleWatch:stream', stream);
+                    init();
+                }
+            });
+        }
+    };
+});
